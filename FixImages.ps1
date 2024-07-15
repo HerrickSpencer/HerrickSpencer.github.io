69d4e77c54972fb6ada095060d9b4b14a7258d72
Herrick Spencer <herricks@microsoft.com>
2024-07-15 11:21:54 -0700

    move cancer posts to unpublished

--- FixImages.ps1
+++ FixImages.ps1
@@ -1,26 +1,29 @@
 ﻿$dir = "D:\source\repos\HerrickSpencer.github.io\_drafts"
-Get-ChildItem -File $dir | ForEach-Object {
+$children = Get-ChildItem -File $dir 
+foreach( $_ in $children) {
     $content = Get-Content $_.FullName
     $changed = $false;
+    
     if ($content -like "*herrickspencer.wordpress.com*")
     {
-        $changed = $true;
+    $_.FullName;
         $content = $content | %{
-            if ($_ -match '<a href=\"https?://herrickspencer.wordpress.com/(\d{4})\/(\d{2})\/(\d{2})\/(.*?)\/\">(.*?)<\/a>')
+            if ($_ -match '<a .*?href=\"https?:\/\/herrickspencer\.wordpress\.com/(\d{4}\/)?(\d{2}\/)?(\d{2}\/)?(.*?)\/?\">(.*?)<\/a>')
             {
+                $changed = $true;
                 $_ = $_ -replace $Matches[0], ("[{0}](/_posts/{1})" -f $Matches[5], $Matches[4]);
-                $_;
             }
-            else
-            {$_;}
-        }
-        $content | %{
-            if ($_ -match '<a href=\"https?://herrickspencer.wordpress.com/(\d{4})\/(\d{2})\/(\d{2})\/(.*?)\/\">(.*?)<\/a>')
-            {
-                ("[{0}](/_posts/{1})" -f $Matches[5], $Matches[4]);
-            }
+            $_;             
         }
     }
+<#    if ($changed -eq $true)
+    {
+        Write-Host "CONTENT:" $content;
+        break;
+        }
+    continue;
+    Write-host "HERE"
+    #>
     if ($content -like "*wp-content*") {
         $changed = $true;    
         $content = $content -replace "https?://herrickspencer.blog/wp-content/uploads", "/{{ site.postMedia }}"
